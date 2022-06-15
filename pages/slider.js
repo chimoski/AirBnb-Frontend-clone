4453122c82712a196ee9d35d4d4c745d4d836bf8
chimoski <chimaobiprince867@gmail.com>
2022-06-15 15:39:34 +0100

    .

--- pages/slider.js
+++ pages/slider.js
@@ -1,53 +1,53 @@
-import React, { useRef, useState } from "react";
-import Image from "next/dist/client/image"
+// import React, { useRef, useState } from "react";
+// import Image from "next/dist/client/image"
 
-import { Swiper, SwiperSlide } from "swiper/react";
+// import { Swiper, SwiperSlide } from "swiper/react";
 
-// Import Swiper styles
-import "swiper/css";
+// // Import Swiper styles
+// import "swiper/css";
 
-export default function App({dummyData}) {
-    const [swiperRef, setSwiperRef] = useState(null);
+// export default function App({dummyData}) {
+//     const [swiperRef, setSwiperRef] = useState(null);
 
   
   
   
-    return (
+//     return (
       
        
-       <>
+//        <>
     
-      <Swiper watchSlidesProgress={true} slidesPerView={3} className="mySwiper">
+//       <Swiper watchSlidesProgress={true} slidesPerView={3} className="mySwiper">
        
-      {dummyData.map(({img, title}, i)=>(
-               <SwiperSlide> 
-                 <div className=" cursor-pointer hover:scale-105  transform  transition duration-200  ease-out">
-          <div className="relative h-80 w-80 ">
-          <Image src={img}
-         layout="fill"
-         className="rounded-xl"
-         alt="image"
-          /> 
+//       {dummyData.map(({img, title}, i)=>(
+//                <SwiperSlide> 
+//                  <div className=" cursor-pointer hover:scale-105  transform  transition duration-200  ease-out">
+//           <div className="relative h-80 w-80 ">
+//           <Image src={img}
+//          layout="fill"
+//          className="rounded-xl"
+//          alt="image"
+//           /> 
 
-          </div>
-        <h3>{title}</h3> <br /> <br />
-          </div>
-                 </SwiperSlide>
-            ))}
+//           </div>
+//         <h3>{title}</h3> <br /> <br />
+//           </div>
+//                  </SwiperSlide>
+//             ))}
        
-      </Swiper>
-    </>
-  );
+//       </Swiper>
+//     </>
+//   );
 
     
-}
+// }
 
 
-export async function getStaticProps (){
-    const dummyData = await fetch('https://links.papareact.com/zp1')
-    .then(res=> res.json());
+// export async function getStaticProps (){
+//     const dummyData = await fetch('https://links.papareact.com/zp1')
+//     .then(res=> res.json());
 
-    return {
-        props:{dummyData}
-    }
-}
+//     return {
+//         props:{dummyData}
+//     }
+// }
